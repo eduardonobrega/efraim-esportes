d1aac5f43e0192cbda7a23cae936173008598356
eduardo <eduardo.nobrega1999@hotmail.com>
2023-10-09 17:23:26 -0300

    initial config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,30 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { globalStyles } from '@/styles/global'
+import { Container, Header, ShoppingCartLink } from '@/styles/pages/app'
+import Image from 'next/image'
+import logoImg from '@/assets/logo.svg'
+import cartImg from '@/assets/shopping-cart.svg'
+import Link from 'next/link'
+
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Link href="/">
+          <Image src={logoImg} width={166} height={62} alt="" />
+        </Link>
+        <ShoppingCartLink href={`/purchase`}>
+          <Image
+            src={cartImg}
+            width={26}
+            height={26}
+            alt="Carrinho de compras branco"
+          />
+        </ShoppingCartLink>
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
